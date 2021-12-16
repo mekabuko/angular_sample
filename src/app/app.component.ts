5f70a86e9570ab2e73322291c575eae4bd9e2d27
mekabuko <36804514+mekabuko@users.noreply.github.com>
2021-12-17 00:48:14 +0900

    fork portal simple project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, Inject, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {POPOUT_MODAL_DATA, POPOUT_MODALS, PopoutData, PopoutModalName} from './popout.tokens';
+import {PopoutService} from './popout.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,98 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular_sample';
+  customerDetails: any = {};
+  companyDetails: any = {};
+
+  constructor(private popoutService: PopoutService) {
+    this.customerDetails = {
+      'Jessica': {
+        id: '1111',
+        age: 35,
+        employer: 'ABCD Bank'
+      },
+      'Mark': {
+        id: '2222',
+        age: 45,
+        employer: 'XY Holdings'
+      },
+    };
+    this.companyDetails = {
+      'ABCD Bank': {
+        id: 2020,
+        founded: 1982,
+        employeeCount: 20000,
+        description: 'ABCD Bank provides wide variety of innovative banking products and services. ' +
+          'We are located in several regions of California and Florida. '
+      },
+      'XY Holdings': {
+        id: 2021,
+        founded: 2000,
+        employeeCount: 10000,
+        description: 'XY Holdings Ltd. provides hosting services, custom software solutions, and an import/export division.'
+      }
+    };
+  }
+
+  @HostListener('window:beforeunload', ['$event'])
+  onWindowClose(event: Event) {
+    this.popoutService.closePopoutModal();
+  }
+
+  openCustomerPopout(name: string) {
+    const modalData = {
+      modalName: PopoutModalName.customerDetail,
+      name: name,
+      id: this.customerDetails[name].id,
+      age: this.customerDetails[name].age,
+      employer: this.customerDetails[name].employer
+    };
+
+    const customerPopoutDetails = POPOUT_MODALS[PopoutModalName.customerDetail];
+
+    if (!this.popoutService.isPopoutWindowOpen()) {
+      this.popoutService.openPopoutModal(modalData);
+    } else {
+      const sameCustomer = POPOUT_MODALS['componentInstance'].name === name;
+      // When popout modal is open and there is no change in data, focus on popout modal
+      if (sameCustomer) {
+        this.popoutService.focusPopoutWindow();
+      } else {
+        POPOUT_MODALS['outlet'].detach();
+        const injector = this.popoutService.createInjector(modalData);
+        const componentInstance = this.popoutService.attachCustomerContainer(POPOUT_MODALS['outlet'], injector);
+        POPOUT_MODALS['componentInstance'] = componentInstance;
+        this.popoutService.focusPopoutWindow();
+      }
+    }
+  }
+
+  openEmployerPopout(name: string) {
+    const modalData = {
+      modalName: PopoutModalName.employerDetail,
+      name: name,
+      id: this.companyDetails[name].id,
+      description: this.companyDetails[name].description,
+      founded: this.companyDetails[name].founded,
+      employeeCount: this.companyDetails[name].employeeCount
+    };
+
+    const employerPopoutDetails = POPOUT_MODALS[PopoutModalName.employerDetail];
+
+    if (!this.popoutService.isPopoutWindowOpen()) {
+      this.popoutService.openPopoutModal(modalData);
+    } else {
+      const sameCustomer = POPOUT_MODALS['componentInstance'].name === name;
+      // When popout modal is open and there is no change in data, focus on popout modal
+      if (sameCustomer) {
+        this.popoutService.focusPopoutWindow();
+      } else {
+        POPOUT_MODALS['outlet'].detach();
+        const injector = this.popoutService.createInjector(modalData);
+        const componentInstance = this.popoutService.attachEmployerContainer(POPOUT_MODALS['outlet'], injector);
+        POPOUT_MODALS['componentInstance'] = componentInstance;
+        this.popoutService.focusPopoutWindow();
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { PortalModule } from '@angular/cdk/portal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { EmployerComponent } from './employer/employer.component';
+import { PopoutService } from './popout.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, PortalModule],
+  exports: [CustomerComponent, EmployerComponent],
+  entryComponents: [CustomerComponent, EmployerComponent],
+  declarations: [AppComponent, CustomerComponent, EmployerComponent],
+  providers: [PopoutService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
